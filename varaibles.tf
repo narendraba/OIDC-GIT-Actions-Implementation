393ef884a0940ac9affb83a1d1f07a250a53620a
narendraba <ballipallinarendra@gmail.com>
2024-12-20 14:40:28 +0530

    Update varaibles.tf

--- varaibles.tf
+++ varaibles.tf
@@ -17,3 +17,23 @@ variable "private_subnet_count" {
 variable "availability_zones" {
   default = ["ap-south-1a", "ap-south-1b"]
 }
+
+variable "ami_id" {
+  default = "ami-0fd05997b4dff7aac"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  default = "desktop"
+}
+
+variable "project_name" {
+  default = "my-project"
+}
